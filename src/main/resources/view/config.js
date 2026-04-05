2c53585ca1ed3f813c07235d031f7a2fae4b7539
Luiz <luiz@jettacargo.com.br>
2026-04-05 13:33:07 -0300

    Add configuration and demo files for viewer; update .gitignore to exclude node_modules

---
+++ src/main/resources/view/config.js
@@ -0,0 +1,6 @@
+import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
+
+// Módulos do viewer do CodinGame usados neste jogo
+export const modules = [
+    GraphicEntityModule
+];
